efd14f80e8530eded5c6156d0bee3be65a00d4e0
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 02:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+var kittens=["Milo", "Otis", "Garfield"]
+
+destructivelyAppendKitten(kittens,1)
+
+function destructivelyAppendKitten(x,y){
+  x.push(y)
+console.log(x)
+
+  return x
+}
+
