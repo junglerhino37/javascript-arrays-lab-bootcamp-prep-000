30ec59c2d2fd50a6ee6433f9787cd83c7b46a5d4
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 02:50:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const app = "I don't do much."
-var kittens=["Milo", "Otis", "Garfield"]
+const app = "I don't do much.";
+var kittens=["Milo", "Otis", "Garfield"];
 
-destructivelyAppendKitten(kittens,1)
+destructivelyAppendKitten(kittens,1);
 
 function destructivelyAppendKitten(x,y){
-  x.push()
-  console.log(x)
-  return x
+  x.push();
+  console.log(x);
+  return x;
 }
 
 function destructivelyPrependKitten(x,y){
-  x.unshift(y)
-  console.log(x)
-  return x
+  x.unshift(y);
+  console.log(x);
+  return x;
 }
